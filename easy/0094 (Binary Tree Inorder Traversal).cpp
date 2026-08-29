462337c7759189fa84050e5b0f9904d21682495e
aarushyadav301 <aarushpyadav@gmail.com>
2026-08-29 00:01:49 -0700

    New submission

---
+++ easy/0094 (Binary Tree Inorder Traversal).cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorderList;
+        if (root == nullptr) {
+            return (inorderList);
+        }
+        inorder(root, inorderList);
+        return (inorderList);
+    }
+
+    void inorder(TreeNode* node, vector<int> &inorderList) {
+        if (node->left) {
+            inorder(node->left, inorderList);
+        }
+        inorderList.push_back(node->val);
+        if (node->right) {
+            inorder(node->right, inorderList);
+        }
+    }
+};
